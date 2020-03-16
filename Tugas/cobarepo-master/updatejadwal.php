29693905d426f47ad34a88d33d973f914e9ec7bf
citrariskyseptiana <60418013+citrariskyseptiana@users.noreply.github.com>
2020-03-16 13:12:51 +0700

    PhpMasjid

---
+++ Tugas/cobarepo-master/updatejadwal.php
@@ -0,0 +1,22 @@
+<?php
+
+include("config.php");
+
+$shubuh = $_POST['shubuh'];
+$dhuhur = $_POST['dhuhur'];
+$ashar = $_POST['ashar'];
+$maghrib = $_POST['maghrib'];
+$isha = $_POST['isha'];
+$dhuha = $_POST['dhuha'];
+ 
+$sql = "UPDATE jadwal_sholat SET shubuh='$shubuh', dhuhur='$dhuhur', ashar='$ashar', maghrib='$maghrib', isha='$isha', dhuha='$dhuha' WHERE id_jadwal=1";
+$query = mysqli_query($db, $sql);
+
+// apakah query update berhasil?
+if( $query ) {
+
+} else {
+	die("Gagal menyimpan perubahan . . .");
+}
+
+?>
