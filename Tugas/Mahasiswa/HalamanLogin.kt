2663a9cf5ea81936d3082823dba08b81c2e38380
citrariskyseptiana <60418013+citrariskyseptiana@users.noreply.github.com>
2020-03-16 13:08:00 +0700

    Mahasiswa

---
+++ Tugas/Mahasiswa/HalamanLogin.kt
@@ -0,0 +1,84 @@
+package com.example.mahasiswa
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
+import kotlinx.android.synthetic.main.activity_dashboard.*
+import kotlinx.android.synthetic.main.activity_halaman_login.*
+import org.json.JSONObject
+
+class HalamanLogin : AppCompatActivity() {
+
+    @SuppressLint("WrongConstant")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_login)
+
+        val recyclerView = findViewById(R.id.recyclerView) as RecyclerView
+        recyclerView.layoutManager= LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+
+        val users=ArrayList<User>()
+
+
+        AndroidNetworking.get("http://192.168.43.191/data_mahasiswa/mahasiswa.php")
+            .setPriority(Priority.MEDIUM)
+            .build()
+            .getAsJSONObject(object : JSONObjectRequestListener {
+                override fun onResponse(response: JSONObject) {
+                    Log.e("_kotlinResponse", response.toString())
+
+                    val jsonArray = response.getJSONArray("result")
+                    for (i in 0 until jsonArray.length()) {
+                        val jsonObject = jsonArray.getJSONObject(i)
+                        Log.e("_kotlinTitle", jsonObject.optString("nama_mhs"))
+
+                        // txt1.setText(jsonObject.optString("shubuh"))
+                        var isi1=jsonObject.optString("nama_mhs").toString()
+                        var isi2=jsonObject.optString("no_mhs").toString()
+                        var isi3=jsonObject.optString("alamat_mhs").toString()
+
+                        users.add(User("$isi1", "$isi2", "$isi3"))
+
+
+                    }
+
+                    val adapter=CustomAdapter(users)
+                    recyclerView.adapter=adapter
+
+
+                }
+
+                override fun onError(anError: ANError) {
+                    Log.i("_err", anError.toString())
+                }
+            })
+
+        val context = this
+
+        btnMhs.setOnClickListener{
+            val intent = Intent(context,Dashboard::class.java)
+            startActivity(intent)
+        }
+
+        btnLogout.setOnClickListener{
+            val sharedPreferences=getSharedPreferences("CEKLOGIN", Context.MODE_PRIVATE)
+            val editor=sharedPreferences.edit()
+
+            editor.putString("STATUS","0")
+            editor.apply()
+
+            startActivity(Intent(this@HalamanLogin,MainActivity::class.java))
+            finish()
+        }
+    }
+}
